d451857aa584b506ca87ebd26aaef8b660989986
shiran.kfir <shiran.kfir1@gmail.com>
2018-03-20 17:52:33 +0200

    remove unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  resources :cuisines
-  resources :reviews
-  resources :restaurants
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-
-Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cuisines
